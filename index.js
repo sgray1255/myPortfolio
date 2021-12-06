9a3f1f7e9b7961e4ea8c46a2fe9f3e3a558187d4
Stephen Gray <stephen_gray1255@yahoo.com>
2021-12-06 11:15:49 +1100

    fixes

---
+++ index.js
@@ -0,0 +1,24 @@
+const phone = document.querySelector('#phone');
+const text = document.querySelector('#text');
+
+phone.addEventListener("mouseover", () => {
+  let alt = phone.alt;
+  text.textContent = alt;
+  text.classList.add("phoneText")
+});
+
+phone.addEventListener("mouseleave", () => {
+  text.textContent = '';
+});
+
+let lastScrollTop;
+navbar = document.querySelector(".header");
+window.addEventListener('scroll',() => {
+let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if(scrollTop > lastScrollTop){
+  navbar.style.top='-80px';
+  } else { 
+  navbar.style.top='0';
+  }
+  lastScrollTop = scrollTop;
+});
